3b68d6a6d6a420abf05d7bb212c08629ba28f498
Adam Naghavi <adamnaghavif@gmail.com>
2024-03-05 02:29:40 -0500

    Removed commented code

--- src/main.c
+++ src/main.c
@@ -6,21 +6,9 @@
 int main()
 {
     /*
-    String str = input('\n',0);
-    char delim[1] = {' '};
-    String_Array a = str_split(str,(String){delim,1});
-
-    for (size_t i = 0; i <a.size; i ++)
-    {
-        printf("%s\n",a.arr[i].cstr);
-    }
-    str_free(str);
-    str = str_arr_join(a, ' ');
-    printf("%s\n",str.cstr);
-    str_arr_free(a);
+    for (int i = 0; i < 10; i++)
+        test_creds();
     */
-    // for (int i = 0; i < 10; i++)
-    //     test_creds();
     shell_loop();
     return 0;
 }
